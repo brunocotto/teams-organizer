1942af56f780088ba931b60f33616a53ae84e100
brunocotto <bruno.ottoo@gmail.com>
2023-04-15 09:12:00 -0300

    created theme, created component loading and applied font style roboto

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
 import { Groups } from '@screens/Groups';
+import { ThemeProvider } from 'styled-components';
+import { Loading } from '@components/Loading';
+import { StatusBar } from 'expo-status-bar';
+
+import theme from '@theme/index';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        translucent
+      />
+      { fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
   );
 }
