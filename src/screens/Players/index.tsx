bd49255e8ee519628c00b9f619f921cd6fca7aad
brunocotto <bruno.ottoo@gmail.com>
2023-04-16 14:42:22 -0300

    created component button icon

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,8 @@
 import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
 
 export function Players() {
   return (
@@ -11,6 +13,13 @@ export function Players() {
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
+
+      <Input
+         placeholder="Nome da pessoa"
+        autoCorrect={false}
+      />
+      
+      <ButtonIcon icon="add"/>
     </Container>
   )
 }
